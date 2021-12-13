996c90ef8f58ef1c1182200feb9ded34c2a53572
Tabbu-Khanam <tabbukhan78612@gmail.com>
2021-12-13 18:08:21 +0530

    changes

--- project5/js/app.js
+++ project5/js/app.js
@@ -1,4 +1,15 @@
 
- baguetteBox.run('.gallery');
-
- 
+ function searchBar() {
+    let input = document.getElementById('search').value;
+    input = input.toLowerCase();
+        let main = document.getElementsByTagName('a');
+     for (i = 0; i < main.length; i++) { 
+        let caption = main[i].getAttribute('data-caption');
+        caption = caption.toLowerCase();
+         if (caption.includes(input)) {
+            main[i].style.display = "block";
+         } else {
+            main[i].style.display = "none";                 
+        }
+     }
+}
