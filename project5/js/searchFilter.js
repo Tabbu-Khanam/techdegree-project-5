e284f899b95daed24ee7752fd61502433cb504cf
Tabbu-Khanam <tabbukhan78612@gmail.com>
2021-12-13 14:35:54 +0530

    Initial commit

---
+++ project5/js/searchFilter.js
@@ -0,0 +1,14 @@
+function searchBar() {
+    let input = document.getElementById('search').value;
+    input = input.toLowerCase();
+        let main = document.getElementsByTagName('a');
+     for (i = 0; i < main.length; i++) { 
+        let caption = main[i].getAttribute('data-caption');
+        caption = caption.toLowerCase();
+         if (caption.includes(input)) {
+            main[i].style.display = "block";
+         } else {
+            main[i].style.display = "none";                 
+        }
+     }
+}
